bf79a0dcbf2df69139128e4cb499c9d6b3274e0d
giorgosathanasopoulos <spzdoom@gmail.com>
2022-10-10 00:15:26 +0300

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../stylesheets/App.css";
+
+export default function App(): JSX.Element {
+    return (
+      <>
+        <h1>Hello, World!</h1>
+      </>
+    );
+};
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Root, createRoot} from "react-dom/client";
+
+import App from "./components/App";
+
+import "./stylesheets/index.css";
+
+const root: Root = createRoot(document.getElementById("root") as HTMLDivElement);
+root.render(<App/>);
